ccbdee3c260e67642c3ca681cfa250bd4cee4cde
Amir Zarrinkafsh <nightah@me.com>
2020-12-23 20:26:32 +1100

    [FEATURE] Add network groups to ACLs
    This change provides the means to define named network groups which can be assigned as networks on specific domain rules per the following examples:
    
    ```yaml
    networks:
      - name: internal
        networks:
          - 10.123.45.0/24
          - 2001:db8:0:45::/64
      - name: VPN
        networks:
          - 10.123.67.0/24
          - 10.123.68.0/24
          - 2001:db8:0:67::/64
          - 2001:db8:0:68::/64
    
    access_control:
      rules:
        - domain: authelia.com
          policy: bypass
          networks:
            - internal
            - VPN
    
        - domain: authelia.com
          resources:
            - "^/executive/.*$"
          policy: two_factor
    
        - domain: internal.authelia.com
          policy: bypass
          networks:
            - internal
            - VPN
    
        - domain: internal.authelia.com
          policy: two_factor
    ```
    
    ACLs are also now validated on launch.

--- internal/authorization/ip_matcher.go
+++ internal/authorization/ip_matcher.go
@@ -3,34 +3,74 @@ package authorization
 import (
 	"net"
 	"strings"
+
+	"github.com/authelia/authelia/internal/configuration/schema"
+	"github.com/authelia/authelia/internal/logging"
 )
 
+func selectMatchingNetworkGroups(networks []string, aclNetworks []schema.ACLNetwork) []schema.ACLNetwork {
+	selectedNetworkGroups := []schema.ACLNetwork{}
+
+	for _, network := range networks {
+		for _, n := range aclNetworks {
+			for _, ng := range n.Name {
+				if network == ng {
+					selectedNetworkGroups = append(selectedNetworkGroups, n)
+				}
+			}
+		}
+	}
+
+	return selectedNetworkGroups
+}
+
+func parseCIDR(ip net.IP, network string) bool {
+	_, ipNet, err := net.ParseCIDR(network)
+	if err != nil {
+		logging.Logger().Errorf("Failed to parse network %s: %s", network, err)
+	}
+
+	if ipNet.Contains(ip) {
+		return true
+	}
+
+	return false
+}
+
 // isIPMatching check whether user's IP is in one of the network ranges.
-func isIPMatching(ip net.IP, networks []string) bool {
+func isIPMatching(ip net.IP, networks []string, aclNetworks []schema.ACLNetwork) bool {
 	// If no network is provided in the rule, we match any network
 	if len(networks) == 0 {
 		return true
 	}
 
+	matchingNetworkGroups := selectMatchingNetworkGroups(networks, aclNetworks)
+
 	for _, network := range networks {
-		if !strings.Contains(network, "/") {
-			if ip.String() == network {
-				return true
+		switch {
+		case ip.String() == network:
+			return true
+		case strings.Contains(network, "/"):
+			if !parseCIDR(ip, network) {
+				continue
 			}
 
-			continue
-		}
-
-		_, ipNet, err := net.ParseCIDR(network)
-
-		if err != nil {
-			// TODO(c.michaud): make sure the rule is valid at startup to
-			// to such a case here.
-			continue
-		}
-
-		if ipNet.Contains(ip) {
 			return true
+		case net.ParseIP(network) == nil && !strings.Contains(network, "/"):
+			for _, n := range matchingNetworkGroups {
+				for _, networks := range n.Networks {
+					switch {
+					case ip.String() == networks:
+						return true
+					case strings.Contains(networks, "/"):
+						if !parseCIDR(ip, networks) {
+							continue
+						}
+
+						return true
+					}
+				}
+			}
 		}
 	}
 
--- internal/authorization/ip_matcher_test.go
+++ internal/authorization/ip_matcher_test.go
@@ -5,19 +5,21 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/authelia/authelia/internal/configuration/schema"
 )
 
 func TestIPMatcher(t *testing.T) {
 	// Default policy is 'allow all ips' if no IP is defined
-	assert.True(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{}))
+	assert.True(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{}, []schema.ACLNetwork{}))
 
-	assert.True(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{"127.0.0.1"}))
-	assert.False(t, isIPMatching(net.ParseIP("127.1"), []string{"127.0.0.1"}))
-	assert.False(t, isIPMatching(net.ParseIP("not-an-ip"), []string{"127.0.0.1"}))
+	assert.True(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{"127.0.0.1"}, []schema.ACLNetwork{}))
+	assert.False(t, isIPMatching(net.ParseIP("127.1"), []string{"127.0.0.1"}, []schema.ACLNetwork{}))
+	assert.False(t, isIPMatching(net.ParseIP("not-an-ip"), []string{"127.0.0.1"}, []schema.ACLNetwork{}))
 
-	assert.False(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{"10.0.0.1"}))
-	assert.False(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{"10.0.0.0/8"}))
+	assert.False(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{"10.0.0.1"}, []schema.ACLNetwork{}))
+	assert.False(t, isIPMatching(net.ParseIP("127.0.0.1"), []string{"10.0.0.0/8"}, []schema.ACLNetwork{}))
 
-	assert.True(t, isIPMatching(net.ParseIP("10.230.5.1"), []string{"10.0.0.0/8"}))
-	assert.True(t, isIPMatching(net.ParseIP("10.230.5.1"), []string{"192.168.0.0/24", "10.0.0.0/8"}))
+	assert.True(t, isIPMatching(net.ParseIP("10.230.5.1"), []string{"10.0.0.0/8"}, []schema.ACLNetwork{}))
+	assert.True(t, isIPMatching(net.ParseIP("10.230.5.1"), []string{"192.168.0.0/24", "10.0.0.0/8"}, []schema.ACLNetwork{}))
 }
